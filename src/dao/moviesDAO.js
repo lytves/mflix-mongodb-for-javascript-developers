483c833108e7fdb511d866ce271f1e17418ecacf
lytves <22356770+lytves@users.noreply.github.com>
2019-04-29 22:42:18 +0200

    Chapter 2: User-Facing Backend, Ticket: Get Comments - RESOLVED

--- src/dao/moviesDAO.js
+++ src/dao/moviesDAO.js
@@ -298,15 +298,38 @@ export default class MoviesDAO {
       stage that searches the `comments` collection for the correct comments.
       */
 
-      // TODO Ticket: Get Comments
+      // RESOLVED - TODO Ticket: Get Comments
       // Implement the required pipeline.
       const pipeline = [
-        {
-          $match: {
-            _id: ObjectId(id)
-          }
-        }
-      ]
+            {
+              $match: {
+                _id: ObjectId(id),
+              },
+            },
+            {
+              $lookup: {
+                from: "comments",
+                let: {
+                  id: "$_id",
+                },
+                pipeline: [
+                  {
+                    $match: {
+                      $expr: {
+                        $eq: ["$movie_id", "$$id"],
+                      },
+                    },
+                  },
+                  {
+                    $sort: {
+                      date: -1,
+                    },
+                  },
+                ],
+                as: "comments",
+              },
+            },
+          ]
       return await movies.aggregate(pipeline).next()
     } catch (e) {
       /**
