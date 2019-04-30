799df7cf5d1b0c42e2a6fb65a73d3b86578c104c
lytves <22356770+lytves@users.noreply.github.com>
2019-04-30 07:54:32 +0200

    Chapter 2: User-Facing Backend, Ticket: Delete Comments - RESOLVED

--- src/dao/commentsDAO.js
+++ src/dao/commentsDAO.js
@@ -98,10 +98,11 @@ export default class CommentsDAO {
     */
 
     try {
-      // TODO Ticket: Delete Comments
+      // RESOLVED - TODO Ticket: Delete Comments
       // Use the userEmail and commentId to delete the proper comment.
       const deleteResponse = await comments.deleteOne({
         _id: ObjectId(commentId),
+        email: userEmail
       })
 
       return deleteResponse
