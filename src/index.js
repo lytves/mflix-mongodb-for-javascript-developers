d22b4e70cc96d4448827cf7e4d7410ae4023b085
lytves <22356770+lytves@users.noreply.github.com>
2019-06-03 17:21:42 +0200

    Chapter 4: Resiliency, Ticket: Connection Pooling - RESOLVED

--- src/index.js
+++ src/index.js
@@ -22,11 +22,13 @@ concern timeout limit to 2500 milliseconds.
 
 MongoClient.connect(
   process.env.MFLIX_DB_URI,
-  // TODO: Connection Pooling
+  // RESOLVED - TODO: Connection Pooling
   // Set the poolSize to 50 connections.
   // TODO: Timeouts
   // Set the write timeout limit to 2500 milliseconds.
-  { useNewUrlParser: true },
+  {
+      poolSize: 50,
+      useNewUrlParser: true },
 )
   .catch(err => {
     console.error(err.stack)
